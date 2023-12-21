9e2a5b0b31f9b00ae9085dff45169f4451f0fd14
akgane <orazgaliahan.af@gmail.com>
2023-12-21 09:50:15 +0600

    Finished program (Finally)

--- src/models/Shape.java
+++ src/models/Shape.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.lang.reflect.Array;
 import java.time.Period;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Shape {
     private ArrayList<Point> points = new ArrayList<Point>();
@@ -16,39 +17,25 @@ public class Shape {
         points.add(point);
     }
 
-    public double calculatePerimeter(){
-        double perimeter = 0;
-        for (int i = 0; i < points.size(); i++) {
-            if(i == (points.size() - 1)){
-                perimeter += points.get(i).getDistance(points.get(0));
-                return perimeter;
-            }
-            perimeter += points.get(i).getDistance(points.get(i + 1));
+    private double[] getSides(){
+        double[] sides = new double[points.size()];
+        for (int i = 0; i < sides.length; i++) {
+            int index = (i + 1) % sides.length;
+            double side = points.get(i).getDistance(points.get(index));
+            sides[i] = side;
         }
-        return perimeter;
+        return sides;
+    }
+
+    public double calculatePerimeter(){
+        return Arrays.stream(getSides()).sum();
     }
 
     public double getLongest(){
-        double max = -Double.MAX_VALUE;
-        for (int i = 0; i < points.size(); i++) {
-            if(i == (points.size() - 1)){
-                if(max < points.get(i).getDistance(points.getFirst())) max = points.get(i).getDistance(points.getFirst());
-                return max;
-            }
-            if(max < points.get(i).getDistance(points.get(i + 1))) max = points.get(i).getDistance(points.get(i + 1));
-        }
-        return max;
+        return Arrays.stream(getSides()).max().getAsDouble();
     }
 
     public double getAverageSide(){
-        double average = 0.0;
-        for (int i = 0; i < points.size(); i++) {
-            if(i == points.size() - 1){
-                average += points.get(i).getDistance(points.getFirst());
-                break;
-            }
-            average += points.get(i).getDistance(points.get(i));
-        }
-        return average / points.size();
+        return Arrays.stream(getSides()).sum() / getSides().length;
     }
 }
